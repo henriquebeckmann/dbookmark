b50e39682433e49c0904b6cf42b7bb3c40261a9e
Henrique Beckmann <henriquebeckmann2@gmail.com>
2023-07-26 23:57:17 -0300

    added search and url support

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@ use std::str;
 use std::env;
 
 fn main() {
-
     let output = Command::new("sh")
         .arg("-c")
-        .arg("bookmark ls | awk 'NF > 0' | dmenu -i -l 12 -p 'bookmark:'")
+        .arg("bookmark ls --sort name | awk 'NF > 0' | dmenu -i -l 12 -p 'bookmark:'")
         .output()
         .expect("failed to execute process");
 
@@ -21,43 +20,54 @@ fn main() {
     }
 
     let output_str = str::from_utf8(&output.stdout).unwrap().trim_end();
-
-    let re = Regex::new("\\s{2,}").unwrap();
-    let fields: Vec<&str> = re.split(output_str).collect();
-
-    let name = fields.get(1).expect("Expected at least 2 fields");
-    let url = fields.get(2).expect("Expected at least 3 fields");
-    let group = fields.get(3).expect("Expected at least 4 fields");
-
+    let url_re = Regex::new(r"^https?://").unwrap();
     let browser = env::var("BROWSER").unwrap_or("firefox".to_string());
 
-    if group == &"search" {
-        let search_term = Command::new("sh")
-            .arg("-c")
-            .arg(format!("echo \"\" | dmenu -p \"{}\":", name))
-            .output()
-            .expect("failed to execute process")
-            .stdout;
+    if output_str == "Id                 Name             URL                                   Group           Tags" {
+        return;
+    }
 
-        let search_term_str = str::from_utf8(&search_term).unwrap().trim_end();
-        let new_url = format!("{}{}", url, search_term_str);
-
-        let status = Command::new(&browser)
-            .arg(&new_url)
+    if url_re.is_match(output_str) {
+        Command::new(&browser)
+            .arg(output_str)
             .status()
             .expect("Failed to launch the browser");
-
-        if !status.success() {
-            println!("Failed to open URL in the browser");
-        }
     } else {
-        let status = Command::new(&browser)
-            .arg(url)
-            .status()
-            .expect("Failed to launch the browser");
+        let re = Regex::new(r"\s{2,}").unwrap();
+        let fields: Vec<&str> = re.split(output_str).collect();
 
-        if !status.success() {
-            println!("Failed to open URL in the browser");
+        if fields.len() < 4 {
+            let search_url = format!("https://duckduckgo.com/?q={}", output_str);
+            Command::new(&browser)
+                .arg(&search_url)
+                .status()
+                .expect("Failed to launch the browser");
+        } else {
+            let name = fields.get(1).expect("Expected at least 2 fields");
+            let url = fields.get(2).expect("Expected at least 3 fields");
+            let group = fields.get(3).expect("Expected at least 4 fields");
+
+            if group == &"search" {
+                let search_term = Command::new("sh")
+                    .arg("-c")
+                    .arg(format!("echo \"\" | dmenu -p \"{}\":", name))
+                    .output()
+                    .expect("failed to execute process")
+                    .stdout;
+
+                let search_term_str = str::from_utf8(&search_term).unwrap().trim_end();
+                let new_url = format!("{}{}", url, search_term_str);
+
+                Command::new(&browser)
+                    .arg(&new_url)
+                    .status()
+                    .expect("Failed to launch the browser");
+            } else {
+                Command::new(&browser)
+                    .arg(url)
+                    .status()
+                    .expect("Failed to launch the browser");
+            }
         }
     }
 }
